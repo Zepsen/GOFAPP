0187663c6c857cc3bdb5a6b32b7a0df093fa4605
Zepsen <zepsendm@gmail.com>
2020-01-19 15:44:32 +0200

    Add project files.

---
+++ FactoryMethod/Oven.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public abstract class Oven
+    {
+        public abstract void Prepare();
+    }
+
+    public class PizzaOven : Oven
+    {
+        public override void Prepare()
+        {
+            Console.WriteLine("Pizza prepared");
+        } 
+    }
+
+    public class BurgerOven : Oven
+    {
+        public override void Prepare()
+        {
+            Console.WriteLine("Burger prepared");
+        } 
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oven = OvenStaticFactory.CreateOven(DishType.Burger);
+            oven.Prepare();
+
+            oven = OvenStaticFactory.CreateOven(DishType.Pizza);
+            oven.Prepare();
+        }
+    }
+}
